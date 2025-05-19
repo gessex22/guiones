d3e5c154856b811a01dda76290fd02f491dd4b7b
gessex22 <gessembergsteiner@gmail.com>
2025-05-18 20:49:46 -0500

    5-18-25

---
+++ los desbloqueadore de celulares.rb
@@ -0,0 +1,35 @@
+los desbloqueadore de celulares
+
+
+
+hoy vamos a hbalr sobre los desbloqueadores de celulares
+en esencia esa persona que le llevas tu celular porque 
+sospechosamente se te olvido el pin 
+
+o el icloud de ios
+
+ustdes sabian que su trabajo realmente no es encontrar la clave
+no es hackear el celular, no es que van a conectarse y meter codigos 
+
+su trabajo realmente es encontrar el programa correcto..
+la aplicaion 
+
+
+y que mis palabras no ofendan a los buenos pero
+
+en resumen su trabajo consiste mirar un tutorial y luego encontrar
+esa herramient  sddsad de dudosa procedencia que hay en internet 
+hasta que den con el correcto que no les cobre una licencia por usarlo
+
+ se dice simple, pero creanme que esos programas son los mas feos que 
+ pueden imaginar. ademas de que siempre ponen una especie de terminal 
+ mostrando comandos como si quienes los usaran suaben que carajos es lo 
+ que sudece internamente  
+
+mis respetos los tienen  los que hacen esos programas que son los que tienen
+los conocieminetnos avanzados, y lo preocupante a la ves es que ellos mismos
+pueden meter troyanos o keyloger o scrips o backdors en esos programas
+
+que utilizan para desbloquear esos celulares 
+ que tu y yo  
+olvidamos la contrrasenia 
